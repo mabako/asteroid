8e4793792025996449ed139056d62744598c66df
Marcus Bauer <mabako@gmail.com>
2011-05-24 11:56:00 +0000

    Kommentare hinzugefügt

--- asteroid/src/game/Game.java
+++ asteroid/src/game/Game.java
@@ -1,7 +1,17 @@
 package game;
 
+/**
+ * Das Spiel
+ * 
+ * @author Marcus Bauer (mabako@gmail.com)
+ * @version 201105041355
+ */
 public class Game implements Runnable
 {
+	/**
+	 * Thread-Funktion
+	 */
+	@Override
 	public void run( )
 	{
 		
